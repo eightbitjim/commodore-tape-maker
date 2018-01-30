31f476d27c79c90ea55d07af547e1f8266d36491
eightbitjim <eightbitjim@gmail.com>
2018-01-30 07:21:36 +0000

    Added more header info

--- tap2wavPY.py
+++ tap2wavPY.py
@@ -5,6 +5,10 @@ samplerate = 44100.0
 duration = 1.0
 frequency = 440.0
 
+shortpulse = 0x30
+mediumpulse = 0x42
+longpulse = 0x56
+
 wavef = 0
 
 def openwavefile(name):
@@ -30,7 +34,66 @@ def addcycle( lengthinseconds ):
 def addtapcycle( tapvalue ):
  addcycle(tapvalue * taplengthinseconds)
 
+def addbit(value):
+ if (value == 0):
+  addtapcycle(shortpulse)
+  addtapcycle(mediumpulse)
+ else:
+  addtapcycle(mediumpulse)
+  addtapcycle(shortpulse)
+
+def adddatamarker(moretofollow):
+ if moretofollow:
+  addtapcycle(longpulse)
+  addtapcycle(mediumpulse)
+ else:
+  addtapcycle(longpulse)
+  addtapcycle(shortpulse)
+
+def addbyteframe(value, moretofollow):
+ checkbit = 1
+ for i in range(8):
+  bit = value & (1 << i)
+  addbit(bit) 
+  checkbit ^= bit
+ addbit(checkbit)
+ adddatamarker(moretofollow)
+ 
+def addleader(type):
+ if (type == 0):
+  numberofpulses = 0x6a00
+ elif (type == 1):
+  numberofpulses = 0x1a00
+ else:
+  numberofpulses = 0x4f
+ for i in range(numberofpulses):
+  addtapcycle( shortpulse )
+
+def addsyncchain(repeated):
+ if repeated:
+  value = 0x09
+ else:
+  value = 0x89
+ while (value > 0):
+  addbyteframe(value, True)
+  value-=1
+
+def addheader(repeated):
+  if (repeated):
+   addleader(2)
+  else:
+   addleader(0)
+  adddatamarker(True)
+  addsyncchain(repeated)
+  pass
+
+def addfile():
+ addleader(1)
+
 openwavefile('test.wav')
-addcycle(0.01)
+
+addheader(False)
+addfile()
+
 closewavefile()
 
