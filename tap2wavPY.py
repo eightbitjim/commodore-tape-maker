3126c4842069cf8d6fc115fa0e73a2a60ae28eaf
eightbitjim <eightbitjim@gmail.com>
2018-02-01 00:18:24 +0000

    Split into classes

--- tap2wavPY.py
+++ tap2wavPY.py
@@ -1,149 +1,184 @@
 import wave, struct, math
 
-taplengthinseconds = 8.0 / 985248.0
-samplerate = 44100.0
-duration = 1.0
-frequency = 440.0
-checksum = 0
+class outputsoundfile:
+ def __init__(self, name):
+  self.name = name
+  self.samplerate = 44100.0
+  self.wavef = 0
+  self.invert = 0
+  self.open()
 
-shortpulse = 0x30
-mediumpulse = 0x42
-longpulse = 0x56
+ def open(self):
+  global wavef;
+  self.wavef = wave.open(self.name, 'w')
+  self.wavef.setnchannels(1)
+  self.wavef.setsampwidth(2)
+  self.wavef.setframerate(self.samplerate)
 
-wavef = 0
-invert = 0
+ def close(self):
+  self.wavef.writeframes('')
+  self.wavef.close()
 
-def openwavefile(name):
- global wavef;
- wavef = wave.open(name, 'w')
- wavef.setnchannels(1)
- wavef.setsampwidth(2)
- wavef.setframerate(44100.0)
+ def addcycle(self, lengthinseconds ):
+  numberofsteps = int(self.samplerate * lengthinseconds)
+  
+  for i in range(numberofsteps):
+   value = int(32767.0 * math.sin(float(i) / float(numberofsteps) * 2.0 * math.pi))
+   if self.invert:
+    value = -value
+   data = struct.pack('<h', value)
+   self.wavef.writeframesraw(data)
 
-def closewavefile():
- global wavef;
- wavef.writeframes('')
- wavef.close()
 
-def addcycle( lengthinseconds ):
- global wavef
- global invert
- numberofsteps = int(samplerate * lengthinseconds)
- for i in range(numberofsteps):
-  value = int(32767.0 * math.sin(float(i) / float(numberofsteps) * 2.0 * math.pi))
-  if invert:
-   value = -value
-  data = struct.pack('<h', value)
-  wavef.writeframesraw(data)
+class commodorefile:
+ def __init__(self, filename):
+  self.taplengthinseconds = 8.0 / 985248.0
+  self.shortpulse = 0x30
+  self.mediumpulse = 0x42
+  self.longpulse = 0x56
+  self.checksum = 0
+  self.data = []
+  self.filenamedata = self.makefilename(filename)
+  self.startaddress = 0
+  self.endaddress = 0
+  self.filetype = 3
+  self.wavefile = 0
 
-def addtapcycle( tapvalue ):
- addcycle(tapvalue * taplengthinseconds)
+ def makefilename(self, filename):
+  return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 
-def addbit(value):
- if (value == 0):
-  addtapcycle(shortpulse)
-  addtapcycle(mediumpulse)
- else:
-  addtapcycle(mediumpulse)
-  addtapcycle(shortpulse)
+ def setcontent(self, inputfile):
+  self.data = inputfile.data
+  self.startaddress = inputfile.startaddress
+  self.endaddress = inputfile.startaddress + len(inputfile.data)
+  self.filetype = inputfile.type
 
-def adddatamarker(moretofollow):
- if moretofollow:
-  addtapcycle(longpulse)
-  addtapcycle(mediumpulse)
- else:
-  addtapcycle(longpulse)
-  addtapcycle(shortpulse)
+ def generatesound(self, outputwavefile):
+  self.wavefile = outputwavefile
+  self.addheader(False)
+  self.addheader(True)
+  self.addfile()
 
-def resetchecksum():
- global checksum
- checksum = 0
-
-def computechecksum():
- global checksum
- return checksum
-
-def addbyteframe(value, moretofollow):
- global checksum
- checkbit = 1
- for i in range(8):
-  if (value & (1 << i)) != 0:
-   bit = 1
-  else:
-   bit = 0
-  addbit(bit) 
-  checkbit ^= bit
- addbit(checkbit)
- adddatamarker(moretofollow)
- checksum ^= value
+ def addtapcycle(self, tapvalue ):
+  wavefile.addcycle(tapvalue * self.taplengthinseconds)
  
-def addleader(type):
- if (type == 0):
-  numberofpulses = 0x6a00
- elif (type == 1):
-  numberofpulses = 0x1a00
- else:
-  numberofpulses = 0x4f
- for i in range(numberofpulses):
-  addtapcycle( shortpulse )
-
-def addsyncchain(repeated):
- if repeated:
-  value = 0x09
- else:
-  value = 0x89
- count = 9
- while (count > 0):
-  addbyteframe(value, True)
-  value-=1
-  count-=1
-
-def adddata(buffer, length):
- for i in range(length):
-  addbyteframe(buffer[i], True)
-
-def addheader(repeated, type, startaddress, endaddress, filenamebytes):
-  if (repeated):
-   addleader(2)
+ def addbit(self, value):
+  if (value == 0):
+   self.addtapcycle(self.shortpulse)
+   self.addtapcycle(self.mediumpulse)
   else:
-   addleader(0)
-  adddatamarker(True)
-  addsyncchain(repeated)
-  resetchecksum()
-  if type != 2:
-   addbyteframe(type, True)
-   addbyteframe(startaddress & 0x00ff, True)
-   addbyteframe((startaddress & 0xff00) >> 8, True)
-   addbyteframe(endaddress & 0x00ff, True)
-   addbyteframe((endaddress & 0xff00) >> 8, True)
-   adddata(filenamebytes, 16)
-  for i in range(171):
-   addbyteframe(0x20, True)
-  addbyteframe(computechecksum(), False)
+   self.addtapcycle(self.mediumpulse)
+   self.addtapcycle(self.shortpulse)
 
-def addfile(filebytes, length):
- repeated = False
- for i in range(2):
-  if (not repeated):
-   addleader(1)
+ def adddatamarker(self, moretofollow):
+  if moretofollow:
+   self.addtapcycle(self.longpulse)
+   self.addtapcycle(self.mediumpulse)
   else:
-   addleader(2)
-  adddatamarker(True)
-  addsyncchain(repeated)
-  resetchecksum()
-  adddata(filebytes, length)
-  addbyteframe(computechecksum(), False)
-  repeated = True
- addleader(1)
+   self.addtapcycle(self.longpulse)
+   self.addtapcycle(self.shortpulse)
 
-openwavefile('test.wav')
+ def resetchecksum(self):
+  self.checksum = 0
+ 
+ def addbyteframe(self, value, moretofollow):
+  checkbit = 1
+  for i in range(8):
+   if (value & (1 << i)) != 0:
+    bit = 1
+   else:
+    bit = 0
+   self.addbit(bit) 
+   checkbit ^= bit
+   
+  self.addbit(checkbit)
+  self.adddatamarker(moretofollow)
+  self.checksum ^= value
+ 
+ def addleader(self, type):
+  if (type == 0):
+   numberofpulses = 0x6a00
+  elif (type == 1):
+   numberofpulses = 0x1a00
+  else:
+   numberofpulses = 0x4f
+   
+  for i in range(numberofpulses):
+   self.addtapcycle( self.shortpulse )
 
-filename = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-filedata = [65, 66, 67, 68]
+ def addsyncchain(self, repeated):
+  if repeated:
+   value = 0x09
+  else:
+   value = 0x89
 
-addheader(False, 3, 1024, 1100, filename)
-addheader(True, 3, 1024, 1100, filename)
-addfile(filedata, 4)
+  count = 9
+  while (count > 0):
+   self.addbyteframe(value, True)
+   value-=1
+   count-=1 
+ 
+ def adddata(self):
+  for i in range(len(self.data)):
+   self.addbyteframe(self.data[i], True)
 
-closewavefile()
+ def addfilename(self):
+  for i in range(len(self.filenamedata)):
+   self.addbyteframe(self.filenamedata[i], True)
+  
+ def addheader(self, repeated):
+   if (repeated):
+    self.addleader(2)
+   else:
+    self.addleader(0)
+    
+   self.adddatamarker(True)
+   self.addsyncchain(repeated)
+   self.resetchecksum()
+   
+   self.addbyteframe(self.filetype, True)
+   self.addbyteframe(self.startaddress & 0x00ff, True)
+   self.addbyteframe((self.startaddress & 0xff00) >> 8, True)
+   self.addbyteframe(self.endaddress & 0x00ff, True)
+   self.addbyteframe((self.endaddress & 0xff00) >> 8, True)
+   self.addfilename()
+   
+   for i in range(171):
+    self.addbyteframe(0x20, True)
+    
+   self.addbyteframe(self.checksum, False)
 
+ def addfile(self):
+  repeated = False
+  for i in range(2):
+   if (not repeated):
+    self.addleader(1)
+   else:
+    self.addleader(2)
+    
+   self.adddatamarker(True)
+   self.addsyncchain(repeated)
+   self.resetchecksum()
+   self.adddata()
+   
+   self.addbyteframe(self.checksum, False)
+   repeated = True
+   
+  self.addleader(1) 
+ 
+ 
+class inputprgfile:
+ def __init__(self, filename):
+  self.data = [1,2,3,4,5,6,7]
+  self.type = 3
+  self.startaddress = 1024
+
+prgfile = inputprgfile('game.prg')
+
+wavefile = outputsoundfile('test.wav')
+
+c64file = commodorefile('test')
+c64file.setcontent(prgfile)
+c64file.generatesound(wavefile)
+
+wavefile.close()
